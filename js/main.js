cf936f2918ea822b36cf3073034ca3293eec4d54
dimonwhite <gen-tver@yandex.ru>
2018-11-25 19:25:57 +0300

    fifth commit

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function append(parent, el){
   return parent.appendChild(el);
 }
 
-function getAlbum(number_album, gallery){
+function getGallery(number_album, gallery){
 
   const url = 'http://jsonplaceholder.typicode.com/albums/';
 
@@ -19,79 +19,72 @@ function getAlbum(number_album, gallery){
     gallery = "gallery";
 
   var gallery_container = document.querySelector('.'+gallery);
-
   var album_container = gallery_container.querySelector('.album');
-
   var title = gallery_container.querySelector('.title');
-
   var button_next = gallery_container.querySelector('.button_next_album');
-
   var button_prev = gallery_container.querySelector('.button_prev_album');
 
   album_container.innerHTML = '';
 
-  fetch(url)
-  .then(function(response){
-    return response.json();
-  })
-  .then(function(data){
-    count_albums = data.length;
-  })
-  .catch(function(error) {
-    title.innerHTML = "Ошибка подключения к галереи!";
-    title.classList.add('error');
-  });
+  function getTitleAlbum(){
+    fetch(url)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(data){
+      count_albums = data.length;
 
-  fetch(url + number_album)
-  .then(function(response){
-    return response.json();
-  })
-  .then(function(data){
-      title.innerHTML = data.title + ' #' + data.id;
+      title.innerHTML = data[number_album-1].title + ' #' + number_album;
 
       button_prev.onclick = function(){
-        if(data.id == 1)
-          getAlbum(count_albums, gallery);
+        if(number_album == 1)
+          getGallery(count_albums, gallery);
         else
-          getAlbum(data.id - 1, gallery);
+          getGallery(number_album - 1, gallery);
       };
       button_next.onclick = function(){
-        if(data.id == count_albums)
-          getAlbum(1, gallery);
+        if(number_album == count_albums)
+          getGallery(1, gallery);
         else
-          getAlbum(data.id + 1, gallery);
+          getGallery(number_album + 1, gallery);
       };
-  })
-  .catch(function(error) {
-    title.innerHTML = "Ошибка подключения к галереи!";
-    title.classList.add('error');
-  });
-
-  fetch(url + number_album + '/photos')
-  .then(function(response){
-    return response.json();
-  })
-  .then(function(data){
-
-    return data.map(function(image){
-
-      var block = createNode('div'),
-      img = createNode('img');
-
-      block.classList.add('album_image');
-      img.src = image.thumbnailUrl;
-      img.setAttribute('data-fullimage', image.url);
-      img.onclick = function(){
-        openImage(this);
-      };
-      append(block, img);
-      append(album_container, block);
     })
-  })
-  .catch(function(error) {
-    title.innerHTML = "Ошибка подключения к галереи!";
-    title.classList.add('error');
-  });
+    .catch(function(error) {
+      title.innerHTML = "Ошибка подключения к галереи!";
+      title.classList.add('error');
+    });
+  };
+
+  function getAlbum(){
+    fetch(url + number_album + '/photos')
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(data){
+
+      return data.map(function(image){
+
+        var block = createNode('div'),
+        img = createNode('img');
+
+        block.classList.add('album_image');
+        img.src = image.thumbnailUrl;
+        img.setAttribute('data-fullimage', image.url);
+        img.onclick = function(){
+          openImage(this);
+        };
+        append(block, img);
+        append(album_container, block);
+      })
+    })
+    .catch(function(error) {
+      title.innerHTML = "Ошибка подключения к галереи!";
+      title.classList.add('error');
+    });
+  };
+
+  getTitleAlbum();
+  getAlbum();
 }
 
 function openImage(item){
@@ -104,4 +97,4 @@ document.querySelector('.overlay').onclick = function(){
   this.classList.remove('active');
 }
 
-getAlbum();
+getGallery();
